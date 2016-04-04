7cf35a5c6371d83130623be4e7a63cbab41eafbb
Peter Bakondy <pbakondy@gmail.com>
2016-04-04 16:38:32 +0200

    Apply JSCS

--- build.js
+++ build.js
@@ -2,10 +2,10 @@
 
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
-const iconv = require('iconv-lite');
+const fs = require( 'fs' );
+const path = require( 'path' );
+const axios = require( 'axios' );
+const iconv = require( 'iconv-lite' );
 
 // https://support.google.com/googleplay/android-developer/answer/6154891?hl=en
 // https://support.google.com/googleplay/answer/1727131?hl=en
@@ -15,41 +15,41 @@ const iconv = require('iconv-lite');
 
 const SUPPORTED_DEVICES_CSV = 'https://storage.googleapis.com/play_public/supported_devices.csv';
 
-const DEVICES_OUTPUT_FILE = path.join(__dirname, 'devices.json');
-const BRANDS_OUTPUT_FILE = path.join(__dirname, 'brands.json');
+const DEVICES_OUTPUT_FILE = path.join( __dirname, 'devices.json' );
+const BRANDS_OUTPUT_FILE = path.join( __dirname, 'brands.json' );
 
 const CVS_SEPARATOR = ',';
 const CVS_QUOTE = '"';
 const CVS_ESCAPE = '"';
 const CVS_NEWLINE = '\r\n';
 
-const CVS_SEPARATOR_CODE = new Buffer(CVS_SEPARATOR)[0];
-const CVS_QUOTE_CODE = new Buffer(CVS_QUOTE)[0];
-const CVS_ESCAPE_CODE = new Buffer(CVS_ESCAPE)[0];
+const CVS_SEPARATOR_CODE = new Buffer( CVS_SEPARATOR )[0];
+const CVS_QUOTE_CODE = new Buffer( CVS_QUOTE )[0];
+const CVS_ESCAPE_CODE = new Buffer( CVS_ESCAPE )[0];
 
 
-function getCvs() {
-  console.log('Downloading: ' + SUPPORTED_DEVICES_CSV);
+function getCvs () {
+  console.log( 'Downloading: ' + SUPPORTED_DEVICES_CSV );
 
-  axios.get(SUPPORTED_DEVICES_CSV)
-    .then(buildJSON)
-    .catch(e => {
-      console.log('Download failed');
-      console.log(e);
-    });
+  axios.get( SUPPORTED_DEVICES_CSV )
+    .then( buildJSON )
+    .catch( e => {
+      console.log( 'Download failed' );
+      console.log( e );
+    } );
 }
 
 // convert utf-16le to utf8
-function decodeRaw(data) {
+function decodeRaw ( data ) {
   // check file encoding:
   // $ file -I supported_devices.csv
   // supported_devices.csv: text/plain; charset=utf-16le
 
-  let buffer = new Buffer(data, 'binary');
-  return iconv.decode(buffer, 'utf-16le');
+  let buffer = new Buffer( data, 'binary' );
+  return iconv.decode( buffer, 'utf-16le' );
 }
 
-function buildJSON(response) {
+function buildJSON ( response ) {
 
 //  console.log(response.data);
 //  console.log(response.status);
@@ -57,79 +57,85 @@ function buildJSON(response) {
 //  console.log(response.headers);
 //  console.log(response.config);
 
-  console.log('Downloaded', response.status, response.statusText);
-  console.log('File size: ' + response.headers['content-length'] + ' bytes');
+  console.log( 'Downloaded', response.status, response.statusText );
+  console.log( 'File size: ' + response.headers['content-length'] + ' bytes' );
 
-  let devices = parseCVS(decodeRaw(response.data));
+  let devices = parseCVS( decodeRaw( response.data ) );
 
   // remove first row: "Retail Branding,Marketing Name,Device,Model"
   devices.shift();
 
-  console.log(devices.length + ' devices');
+  console.log( devices.length + ' devices' );
 
-  fs.writeFile(DEVICES_OUTPUT_FILE, JSON.stringify(devices, null, 2), (err) => {
-    if (err) throw err;
-    console.log('Device list saved to ' + DEVICES_OUTPUT_FILE);
-  });
+  fs.writeFile( DEVICES_OUTPUT_FILE, JSON.stringify( devices, null, 2 ), ( err ) => {
+    if ( err ) {
+      throw err;
+    }
+    console.log( 'Device list saved to ' + DEVICES_OUTPUT_FILE );
+  } );
 
   let brands = [];
-  devices.forEach(device => {
-    if (!device.brand) {
+  devices.forEach( device => {
+    if ( !device.brand ) {
       return;
     }
-    if (brands.indexOf(device.brand) === -1) {
-      brands.push(device.brand);
+    if ( brands.indexOf( device.brand ) === -1 ) {
+      brands.push( device.brand );
     }
-  });
+  } );
 
-  console.log(brands.length + ' brands');
+  console.log( brands.length + ' brands' );
 
-  fs.writeFile(BRANDS_OUTPUT_FILE, JSON.stringify(brands, null, 2), (err) => {
-    if (err) throw err;
-    console.log('Brand list saved to ' + BRANDS_OUTPUT_FILE);
-  });
+  fs.writeFile( BRANDS_OUTPUT_FILE, JSON.stringify( brands, null, 2 ), ( err ) => {
+    if ( err ) {
+      throw err;
+    }
+    console.log( 'Brand list saved to ' + BRANDS_OUTPUT_FILE );
+  } );
 
 }
 
-function parseCVS(data) {
-  let dataArr = data.split(CVS_NEWLINE);
+function parseCVS ( data ) {
+  let dataArr = data.split( CVS_NEWLINE );
   let out = [];
 
-  dataArr.map(item => {
-    let parts = splitRow(item);
-    if (parts) {
-      out.push({
+  dataArr.map( item => {
+    let parts = splitRow( item );
+    if ( parts ) {
+      out.push( {
         brand: parts[0],
         name: parts[1],
         device: parts[2],
         model: parts[3]
-      });
+      } );
     }
-  });
+  } );
 
   return out;
 }
 
-function splitRow(row) {
+function splitRow ( row ) {
   let parts;
-  if ((row.indexOf(CVS_QUOTE) > -1) || (row.indexOf(CVS_ESCAPE) > -1)) {
-    parts = splitSpecial(row);
+  if ( ( row.indexOf( CVS_QUOTE ) > -1 ) || ( row.indexOf( CVS_ESCAPE ) > -1 ) ) {
+    parts = splitSpecial( row );
   } else {
-    parts = row.split(CVS_SEPARATOR);
+    parts = row.split( CVS_SEPARATOR );
   }
-  if (row.trim().length === 0) {
+  if ( row.trim().length === 0 ) {
     // empty row
     return;
   }
-  if (parts.length !== 4) {
-    console.log('Invalid row:', row, row.length, parts);
+  if ( parts.length !== 4 ) {
+    console.log( 'Invalid row:', row, row.length, parts );
     return;
   }
   return parts;
 }
 
-function splitSpecial(data) {
-  if (typeof data === 'string') data = new Buffer(data);
+function splitSpecial ( data ) {
+  if ( typeof data === 'string' ) {
+    data = new Buffer( data );
+  }
 
   let start = 0;
   let end = data.length;
@@ -137,36 +143,40 @@ function splitSpecial(data) {
   let isQuoted = false;
   let offset = start;
 
-  for (let i = start; i < end; i++) {
+  for ( let i = start; i < end; i++ ) {
     let isStartingQuote = !isQuoted && data[i] === CVS_QUOTE_CODE;
     let isEndingQuote = isQuoted && data[i] === CVS_QUOTE_CODE && i + 1 <= end && data[i + 1] === CVS_SEPARATOR_CODE;
     let isEscape = isQuoted && data[i] === CVS_ESCAPE_CODE && i + 1 < end && data[i + 1] === CVS_QUOTE_CODE;
 
-    if (isStartingQuote || isEndingQuote) {
+    if ( isStartingQuote || isEndingQuote ) {
       isQuoted = !isQuoted;
       continue;
-    } else if (isEscape) {
+    } else if ( isEscape ) {
       i++;
       continue;
     }
 
-    if (data[i] === CVS_SEPARATOR_CODE && !isQuoted) {
-      cells.push(cell(data, offset, i));
+    if ( data[i] === CVS_SEPARATOR_CODE && !isQuoted ) {
+      cells.push( cell( data, offset, i ) );
       offset = i + 1;
     }
   }
 
-  if (offset < end) cells.push(cell(data, offset, end));
-  if (data[end - 1] === CVS_SEPARATOR_CODE) cells.push('');
+  if ( offset < end ) {
+    cells.push( cell( data, offset, end ) );
+  }
+  if ( data[end - 1] === CVS_SEPARATOR_CODE ) {
+    cells.push( '' );
+  }
 
   return cells;
 }
 
-function cell(data, start, end) {
+function cell ( data, start, end ) {
   // remove quotes from quoted cells
   let quotes = 0;
 
-  if (data[start] === CVS_QUOTE_CODE && data[end - 1] === CVS_QUOTE_CODE) {
+  if ( data[start] === CVS_QUOTE_CODE && data[end - 1] === CVS_QUOTE_CODE ) {
     start++;
     end--;
     quotes = 1;
@@ -174,14 +184,18 @@ function cell(data, start, end) {
 
   let i = start;
   let y = start;
-  for (; i < end; i++) {
+  for ( ; i < end; i++ ) {
     // check for escape characters and skip them
-    if (data[i] === CVS_ESCAPE_CODE && i + 1 < end && data[i + 1] === CVS_QUOTE_CODE) i++;
-    if (y !== i) data[y] = data[i];
+    if ( data[i] === CVS_ESCAPE_CODE && i + 1 < end && data[i + 1] === CVS_QUOTE_CODE ) {
+      i++;
+    }
+    if ( y !== i ) {
+      data[y] = data[i];
+    }
     y++;
   }
 
-  return data.toString('utf-8', start, end - quotes);
+  return data.toString( 'utf-8', start, end - quotes );
 }
 
 
--- test.js
+++ test.js
@@ -1,37 +1,37 @@
 import test from 'ava';
 
-const devices = require('./index');
+const devices = require( './index' );
 
-test('deviceList()', t => {
-  t.is(typeof devices.deviceList, 'function');
-  t.ok(Array.isArray(devices.deviceList()));
-});
+test( 'deviceList()', t => {
+  t.is( typeof devices.deviceList, 'function' );
+  t.ok( Array.isArray( devices.deviceList() ) );
+} );
 
-test('brandList()', t => {
-  t.is(typeof devices.brandList, 'function');
-  t.ok(Array.isArray(devices.brandList()));
-});
+test( 'brandList()', t => {
+  t.is( typeof devices.brandList, 'function' );
+  t.ok( Array.isArray( devices.brandList() ) );
+} );
 
-test('getDevicesByBrand()', t => {
-  t.is(typeof devices.getDevicesByBrand, 'function');
-  t.throws(function() { devices.getDevicesByBrand() });
-  t.ok(Array.isArray(devices.getDevicesByBrand('')));
-});
+test( 'getDevicesByBrand()', t => {
+  t.is( typeof devices.getDevicesByBrand, 'function' );
+  t.throws( function () { devices.getDevicesByBrand(); } );
+  t.ok( Array.isArray( devices.getDevicesByBrand( '' ) ) );
+} );
 
-test('getDevicesByName()', t => {
-  t.is(typeof devices.getDevicesByName, 'function');
-  t.throws(function() { devices.getDevicesByName() });
-  t.ok(Array.isArray(devices.getDevicesByName('')));
-});
+test( 'getDevicesByName()', t => {
+  t.is( typeof devices.getDevicesByName, 'function' );
+  t.throws( function () { devices.getDevicesByName(); } );
+  t.ok( Array.isArray( devices.getDevicesByName( '' ) ) );
+} );
 
-test('getDevicesByDeviceId()', t => {
-  t.is(typeof devices.getDevicesByDeviceId, 'function');
-  t.throws(function() { devices.getDevicesByDeviceId() });
-  t.ok(Array.isArray(devices.getDevicesByDeviceId('')));
-});
+test( 'getDevicesByDeviceId()', t => {
+  t.is( typeof devices.getDevicesByDeviceId, 'function' );
+  t.throws( function () { devices.getDevicesByDeviceId(); } );
+  t.ok( Array.isArray( devices.getDevicesByDeviceId( '' ) ) );
+} );
 
-test('getDevicesByModel()', t => {
-  t.is(typeof devices.getDevicesByModel, 'function');
-  t.throws(function() { devices.getDevicesByModel() });
-  t.ok(Array.isArray(devices.getDevicesByModel('')));
-});
+test( 'getDevicesByModel()', t => {
+  t.is( typeof devices.getDevicesByModel, 'function' );
+  t.throws( function () { devices.getDevicesByModel(); } );
+  t.ok( Array.isArray( devices.getDevicesByModel( '' ) ) );
+} );
